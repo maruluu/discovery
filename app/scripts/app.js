994d62db11f2e72add34567c86495348928a9cc9
Addy Osmani <addyosmani@gmail.com>
2013-06-04 14:52:30 +0100

    Adding routes for each view which will have a controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,6 +7,18 @@ angular.module('discoverApp', ['GithubService', 'GithubDirective'])
         templateUrl: 'views/search.html',
         controller: 'MainCtrl'
       })
+      .when('/github/:user',{
+        templateUrl: 'views/user.html',
+        controller: 'UserCtrl'
+      })
+      .when('/github/:user/:repo',{
+        templateUrl: 'views/repository.html',
+        controller: 'RepoCtrl'
+      })
+      .when('/about', {
+        templateUl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
