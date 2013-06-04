e6ec7ffe2e3f3dc766d6aa00aca3b66585cab3c6
Addy Osmani <addyosmani@gmail.com>
2013-06-04 14:35:41 +0100

    adding initial tabs directive

--- app/scripts/directives/Github.js
+++ app/scripts/directives/Github.js
@@ -1,7 +1,9 @@
 'use strict';
 
-angular.module('GithubDirective', [])
-  .directive('tabs', function () {
+angular.module('GithubDirective', []).
+
+
+ directive('tabs', function () {
     return {
     	restrict: 'E',
     	// transclusion consists of plucking out the content of a 
@@ -12,6 +14,32 @@ angular.module('GithubDirective', [])
     	scope: {},
     	controller: function($scope, $element){
     		var panes = $scope.panes = [];
-    	}
-    };
-  });
+
+    		// Pane selection
+    		$scope.select = function(pane){
+    			angular.forEach(panes, function(pane){
+    				pane.selected = false;
+    			});
+
+    			pane.selected = true;
+    		}
+
+    		// Add a new tab pane
+    		this.addPane = function(pane){
+    			if (panes.length === 0){
+    				$scope.select(pane);
+    				panes.push(pane);
+    			}
+    		},
+    		template:
+    		'<div class="tabbable">' + 
+    			'<ul class="nav nav-tabs">' + 
+    				'<li ng-repeat="pane in panes ng-class="{active:pane.selected">' + 
+    					'<a href="" ng-click="select(pane)">{{pane.title}}</a>' + 
+    				'</li>' + 
+    				'</ul>' +
+    				'<div class="tab-content" ng-transclude></div>' + 
+    		'</div>',
+    		replace: true
+    	};
+  }).
